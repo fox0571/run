4135bb3e7334b2174390f3bdd6b7851818925f65
胡波 <fox0571@gmail.com>
2016-09-02 16:52:59 +0800

    alpha

--- RunToMusic/ViewController.swift
+++ RunToMusic/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,24 +21,72 @@ class ViewController: UIViewController {
         let url: NSURL! = NSURL(string: listURL)
         let request: NSURLRequest = NSURLRequest(URL: url)
         let session=NSURLSession.sharedSession()
-        let data=session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
-            if error != nil{
-                print(error?.code)
-                print(error?.description)
-            }else{
-                do{
-                    let jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
-                    print(jsonResult)
-                    let str = NSString(data:data!, encoding: NSUTF8StringEncoding)
-                    print("aaaaaaa")
-                    print(str)
-                }catch {
-                    print("json error\(error)")
-                }
-                
-            }
-        })
+        var tempurl:NSURL!
+        let task = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            do {
+                if error != nil{
+                    print(error?.code)
+                    print(error?.description)
+                }else{
+                    let jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as!NSDictionary
+                    let songInfo: NSArray = jsonResult["song"] as! NSArray
+                    for info in songInfo{
+                        var t:String!
+                        t=info["url"] as! String
+                        if t != ""{
+                            print(t+"\n")
+                            tempurl=NSURL(string: t)
+                            print(t)
+                        }
+                    }
+                    
+                    
+                    let configDefault = NSURLSessionConfiguration.defaultSessionConfiguration()
+                    
+                    configDefault.timeoutIntervalForRequest = 20
+                    
+                    let session1 = NSURLSession(configuration: configDefault)
+                    
+                    
+                    
+                    
+                    let dataTask = session1.dataTaskWithURL(tempurl!, completionHandler: {(data2,response,error)->Void in
+                        print(error)
+                        
+                        self.playsong(data2!)
+                        
+                        
+                    })
+                    dataTask.resume()
 
+                    //print(songInfo)
+                        //jsonResult.objectForKey("song")!
+                    //let urlString: String=jsonResult.objectForKey("url") as! String
+                    //let musicURL: NSURL!=NSURL(string: urlString)
+                    //print(si)
+                    //print(songInfo.objectForKey("url"))
+                    //let str = NSString(data:data!, encoding: NSUTF8StringEncoding)
+                    //print(str)
+                }
+            }catch {
+                    print("json error\(error)")
+            }
+            
+        })
+        task.resume()
+        
+
+    }
+    func playsong(musicdata:NSData){
+        do{
+            
+            //try audioPlayer = AVAudioPlayer.init(contentsOfURL: url!)
+            try audioPlayer = AVAudioPlayer(data: musicdata)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
